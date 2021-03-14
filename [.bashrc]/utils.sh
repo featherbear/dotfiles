5fa7aba32ba7d48fb20879142a4e03ce0d6c1d8d
Andrew Wong <andrew.j.wong@outlook.com>
2021-03-14 16:53:52 +1100

    Zerotier

--- [.bashrc]/utils.sh
+++ [.bashrc]/utils.sh
@@ -2,3 +2,6 @@
 alias cat="batcat --tabs 2 --theme OneHalfDark"
 alias batt="upower -i $(upower -e | grep 'BAT')"
 alias wifree="~/.opt/wifree.py"
+
+# github.com/featherbear/zt.py/
+alias zt="sudo ~/.opt/zt.py"
