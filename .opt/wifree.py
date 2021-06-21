98af57c066a0495aed7be551b4a5f26c0bf2fa25
Andrew Wong <andrew.j.wong@outlook.com>
2021-06-21 14:49:34 +1000

    Update wifree.py

--- .opt/wifree.py
+++ .opt/wifree.py
@@ -15,7 +15,7 @@ class downedInterface:
   def __enter__(self):
     os.system(f"sudo ifconfig {self.interface} down")
   def __exit__(self, *_):
-    os.system(f"sudo ifconfig {interface} up")
+    os.system(f"sudo ifconfig {self.interface} up")
 
 def getMAC(interface):
   os.system(f"ifconfig {interface} | grep ether | cut -d ' ' -f 10")
