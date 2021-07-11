b3d6c33129cca58faabaec1a984037e0fa1a6f9d
Andrew Wong <andrew.j.wong@outlook.com>
2021-07-11 22:23:15 +1000

    Update startPwndocker.sh

--- .opt/startPwndocker.sh
+++ .opt/startPwndocker.sh
@@ -4,12 +4,15 @@ echo "Launching pwndocker"
 
 cat > /tmp/.pwnDockerBashAliases << :EOF
 gropper () { ropper -f "\$1" --nocolor | grep "\$2"; }
+alias aslr="setarch \`uname -m\` -R"
 export PS1="\n--------------------\n\w \T \$?\n\\\\$ \[\$(tput sgr0)\]"
 :EOF
 
+pwnDockerSuffix="@sha256:7a91fcee7f34679a40bef80f82a2872298530b0c78c2e0ac9fd183c15fe60ba2"
+
 if [ "$(uname)" == "Darwin" ]; then
   # Mac support
-  docker run -d --rm -h banana --name banana -v /tmp/.pwnDockerBashAliases:/root/.bash_aliases  -v $(pwd):/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined skysider/pwndocker >/dev/null 2>&1
+  docker run -d --rm -h banana --name banana -v /tmp/.pwnDockerBashAliases:/root/.bash_aliases  -v $(pwd):/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined skysider/pwndocker$pwnDockerSuffix >/dev/null 2>&1
   docker exec -it banana /bin/bash
 else
   if ! id -nG | grep -qw docker; then
@@ -19,6 +22,6 @@ else
       exit
   fi
 
-  sg docker 'cd $(pwd); docker run -d --rm -h banana --name banana -v /tmp/.pwnDockerBashAliases:/root/.bash_aliases  -v $(pwd):/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined skysider/pwndocker >/dev/null 2>&1'
+  sg docker 'cd $(pwd); docker run -d --rm -h banana --name banana -v /tmp/.pwnDockerBashAliases:/root/.bash_aliases  -v $(pwd):/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined skysider/pwndocker$pwnDockerSuffix >/dev/null 2>&1'
   sg docker 'docker exec -it banana /bin/bash'
 fi
